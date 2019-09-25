4d49ddef512cf607fcbb0ad35b5761c3b6153580
kalifun <kalifun@163.com>
2019-09-25 20:15:26 +0800

    add v1 api
    add v1 api

---
+++ routers/api/v1/tag.go
@@ -0,0 +1,23 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+//获取多个标签
+func GetTags(c *gin.Context)  {
+	
+}
+
+//增加标签
+func AddTag(c *gin.Context)  {
+	
+}
+
+//修改标签
+func EditTag(c *gin.Context)  {
+
+}
+
+//删除标签
+func DeleteTag(c *gin.Context)  {
+	
+}
--- routers/router.go
+++ routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"kalifun/ginblog/pkg/setting"
+	v1 "kalifun/ginblog/routers/api/v1"
 )
 
 func InitRouter() *gin.Engine {
@@ -14,11 +15,12 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
-	r.GET("/test", func(context *gin.Context) {
-		context.JSON(200,gin.H{
-			"message":"test",
-		})
-	})
-
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.GET("/tags", v1.GetTags)
+		apiv1.POST("/tags", v1.AddTag)
+		apiv1.PUT("/tags/:id", v1.EditTag)
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+	}
 	return r
 }
